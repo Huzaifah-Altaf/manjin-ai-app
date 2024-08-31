21f480511e9da03c5f72c4e7c1d2be3d85d056f8
Huzaifah-Altaf <huzaifahaltaf@gmail.com>
2024-08-31 19:42:29 +0530

    Initial commit with Streamlit app

---
+++ PROJ1.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from groq import Groq
+client = Groq(api_key='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')
+# website name 
+st.title('Manjin AI') 
+st.write('Ask me anything')
+
+def main():
+    message = st.text_input('Write the Question here')
+    if st.button('Submit'):
+        st.write('You asked:', message)
+        chat_completions = client.chat.completions.create(
+            model="llama-3.1-70b-versatile",
+            messages = [
+                {
+                   "role":"user",
+                   'content':message
+
+                }
+                
+            ],      
+
+
+        )
+        if chat_completions:
+            st.write('BOT : '+ chat_completions.choices[0].message.content)
+        else:
+            st.write('No response from the bot')
+if __name__ == '__main__':
+    main()
