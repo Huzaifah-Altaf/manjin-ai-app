7159c315cbc896ea69d8d4d3e78fcb1c2b4fceed
Huzaifah Altaf <huzaifahaltaf@gmail.com>
2025-02-13 13:38:18 +0530

    PROJ1.py

--- PROJ1.py
+++ PROJ1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from groq import Groq
-client = Groq(api_key='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')
+client = Groq(api_key='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')
 
 st.title('Manjin AI') # title 
 st.subheader('Created by Huzaifah Altaf')
